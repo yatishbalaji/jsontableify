64aab8278ee98c9ba007953e11bb7f918f4403b2
Thang Phung <thangphung.work@gmail.com>
2020-05-10 18:04:47 +0000

    add embed feature

--- examples/toHtml.js
+++ examples/toHtml.js
@@ -6,8 +6,11 @@ const Jsontableify = require('../src/jsontableify');
 const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, './example.json')), 'utf-8');
 
 console.log(new Jsontableify({
-  headerList: ['Phone', 'Attachments', 'PersonCompetency'],
-  dateFormat: 'DD-MM-YYYY',
-  replaceTextMap: { YearsOfExperience: 'Years Of Experience' },
-  excludeKeys: ['Current CTC'],
+    headerList: ['Phone', 'Attachments', 'PersonCompetency'],
+    dateFormat: 'DD-MM-YYYY',
+    replaceTextMap: { YearsOfExperience: 'Years Of Experience' },
+    excludeKeys: ['Current CTC'],
+    embeds: [
+        `<link rel="stylesheet" type="text/css" href="https://gitcdn.link/repo/yatishbalaji/jsontableify/master/examples/toHtml.css">`
+    ]
 }).toHtml(json));
